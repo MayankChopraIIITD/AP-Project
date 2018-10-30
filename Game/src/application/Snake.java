274d7ce7c7fe23e8db7602f9504ebf6d5cbf08ad
naman070 <40037252+naman070@users.noreply.github.com>
2018-10-31 00:22:59 +0530

    Add files via upload

---
+++ Game/src/application/Snake.java
@@ -0,0 +1,81 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.geometry.Point2D;
+import java.util.*;
+
+public class Snake extends Application{
+	private Pane root = new Pane();
+	private Sprite player;
+	private ArrayList<Sprite> snake = new ArrayList<>();
+
+	
+	private Parent createContent(){
+		root.setPrefSize(600,800);
+		int x=15;
+		for(int i=0;i<5;i++){
+			player = new Sprite(300,600+(2*x),15,"snake",Color.BLUE);
+			root.getChildren().add(player);
+			x = x+15;
+			snake.add(player);
+		}
+		return root;
+	}
+	
+	
+	
+	@Override
+	public void start(Stage stage) throws Exception{
+		Scene scene = new Scene(createContent());
+		scene.setOnKeyPressed(e -> {
+			switch (e.getCode()){
+				case A:
+					for(int i=0;i<snake.size();i++){
+						snake.get(i).moveLeft();
+					}
+					break;
+				case D:
+					for(int i=0;i<snake.size();i++){
+						snake.get(i).moveRight();
+					}
+					break;
+					
+			}
+		});
+		stage.setScene(scene);	
+		stage.show();
+	}
+	
+	public ArrayList<Sprite> getSnake(){
+		return snake;
+	}
+	
+	
+	private static class Sprite extends Circle {
+		boolean dead = false;
+		final String type;
+		Sprite(int x,int y,int r, String type,Color color){
+			super(r,color);
+			this.type = type;
+			setTranslateX(x);
+			setTranslateY(y);
+		}
+		void moveLeft(){
+			setTranslateX(getTranslateX()-15);			
+		}
+		void moveRight(){
+			setTranslateX(getTranslateX()+15);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
