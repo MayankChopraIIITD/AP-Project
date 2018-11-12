0648bf6491536c93b759d95ecd385d0616dbd392
naman070 <40037252+naman070@users.noreply.github.com>
2018-11-13 01:10:07 +0530

    Update token.java

--- Game/src/application/token.java
+++ Game/src/application/token.java
@@ -1,35 +1,26 @@
 import java.util.*;
-import java.io.*;
 
-
-public class token {
+public abstract class token {
 	private ArrayList<Integer> arr_x;
 	private int y_cor;
 	
 	public token(){
 		arr_x = new ArrayList<>();
 	}
-	public void generate_random_list(){
+	public int get_random_x_coordinate(){
 		int sum_x = 300;
-		for(int i=-19;i<=19;i++){
-			sum_x = 300 + (i*15);
+		for(int i=-9;i<=9;i++){
+			sum_x = 300 + (i*30);
 			arr_x.add(sum_x);
 		}
-	}
-	public ArrayList<Integer> get_shuffled_list(){
 		Collections.shuffle(arr_x);
-		return arr_x;
+		return arr_x.get(arr_x.size()/2);
 	}
 	
-	public void random_y_cordinate(){
+	public int get_random_y_coordinate(){
 		Random num = new Random();
-		y_cor = -700-num.nextInt(300);
-	}
-	public int get_y_coordinate(){
+		y_cor = -700+num.nextInt(300);
 		return y_cor;
 	}
-	
-	
-	
 	
 }
